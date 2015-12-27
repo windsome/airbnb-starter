1551cf90fca1c4a5e3ea47a642db5dda5e74ab09
windsome <36081189@qq.com>
2015-12-27 22:32:38 +0800

    stub ok.

--- app/index.js
+++ app/index.js
@@ -3,15 +3,22 @@ import React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux';
 import { Provider } from 'react-redux'
-import { Router, browserHistory as history } from 'react-router'
+import { Router, browserHistory } from 'react-router'
 import { syncReduxAndRouter } from 'redux-simple-router';
 
-import configureStore from './configureStore'
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+const history = createBrowserHistory();
+//const history = browserHistory;
 
+// Grab the state from a global injected into server-generated HTML
+const initialState = window.__INITIAL_STATE__;
+
+//import configureStore from './configureStore'
 //import routes from './routes';
+import configureStore from './stub/configureStore'
 import routes from './stub/routes';
 
-const store = configureStore()
+const store = configureStore(initialState);
 
 // Installs hooks that always keep react-router and redux store in sync
 syncReduxAndRouter(history, store);
@@ -21,7 +28,7 @@ render(
     <Router history={history} routes={routes} />
   </Provider>,
 
-  document.getElementById('example')
+  document.getElementById('root')
 )
 
 
