e2512fa330ecca8798c23b607874d6c9be025b8e
windsome <36081189@qq.com>
2015-12-27 23:27:14 +0800

    add comment for history.

--- app/index.js
+++ app/index.js
@@ -1,22 +1,24 @@
 /*eslint-disable no-unused-vars */
+//import 'babel-core/polyfill'
+
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux';
+import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import { Router, browserHistory } from 'react-router'
-import { syncReduxAndRouter } from 'redux-simple-router';
-
+import { Router } from 'react-router'
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 const history = createBrowserHistory();
-//const history = browserHistory;
-
-// Grab the state from a global injected into server-generated HTML
-const initialState = window.__INITIAL_STATE__;
+// react-router 1.0.3 not support browserHistory, latest master brunch support. use createBrowserHistory instead temply.
+//import { Router, browserHistory as history } from 'react-router';
+import { syncReduxAndRouter } from 'redux-simple-router'
 
 //import configureStore from './configureStore'
 //import routes from './routes';
 import configureStore from './stub/configureStore'
 import routes from './stub/routes';
+
+// Grab the state from a global injected into server-generated HTML
+const initialState = window.__INITIAL_STATE__;
 
 const store = configureStore(initialState);
 
